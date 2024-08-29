ae4624fd50ec862bb2dd96a83c5771eddce8cb5c
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2024-08-29 07:32:56 +0400

    fgggg

---
+++ VS first time/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter the Number:");
+int x= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(x);
